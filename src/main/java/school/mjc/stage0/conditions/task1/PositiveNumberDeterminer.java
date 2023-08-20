2c2fe2b5b740be607a46febbc163c6a55755d281
Ramaz Artmeladze <ramaz.artmeladze@outlook.com>
2023-08-20 14:57:39 +0400

    update

--- src/main/java/school/mjc/stage0/conditions/task1/PositiveNumberDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task1/PositiveNumberDeterminer.java
@@ -2,5 +2,17 @@ package school.mjc.stage0.conditions.task1;
 
 public class PositiveNumberDeterminer {
     public void isPositive(int numberToBeDetermined) {
+        if (numberToBeDetermined > 0) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+    }
+
+    public static void main(String[] args) {
+        PositiveNumberDeterminer determiner = new PositiveNumberDeterminer();
+        determiner.isPositive(5); // Output will be "true"
+        determiner.isPositive(-3); // Output will be "false"
+        determiner.isPositive(0); // Output will be "false"
     }
 }
